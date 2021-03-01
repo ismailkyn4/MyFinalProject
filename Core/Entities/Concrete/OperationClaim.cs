9e2f3da51a3f3d187fa72e1f360d27df1118f8fe
ismail koyun <iso.king43@gmail.com>
2021-03-01 21:49:13 +0300

    27.02.2021

---
+++ Core/Entities/Concrete/OperationClaim.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities.Concrete
+{
+    public class OperationClaim:IEntity
+    {
+        public int MyProperty { get; set; }
+        public string Name { get; set; }
+
+    }
+}
